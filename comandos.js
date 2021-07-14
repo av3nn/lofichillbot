188b3e341f7c6ac37c400aecfef4b8861318c19c
BM <bruno.mafp@gmail.com>
2021-07-14 13:57:04 -0300

    Desgambiarrando
    alterei a forma como o bot recebe comandos e ainda estou tentando criar canais

---
+++ comandos.js
@@ -0,0 +1,23 @@
+const { prefix, chatname } = require('./config.json');
+
+module.exports = (client, aliases, callback) => {
+    if (typeof aliases === 'string'){
+        aliases = [aliases]
+    }
+     
+    client.on("message", message => {
+        if (message.channel.name != chatname) return;
+
+        const { content } = message;
+
+        aliases.forEach(alias => {
+            const command = `${prefix}${alias}`
+            if (content.startsWith(`${command}`) || content === command ){
+                console.log(`Executando comando: ${command}`);
+                callback(message);
+            }
+        });
+
+
+    }
+)};
--- index.js
+++ index.js
@@ -10,54 +10,36 @@
     */
 
 const Discord = require("discord.js");
-const config = require("./config.json")
-    
+const config = require("./config.json");
+const comando = require("./comandos.js");
+
 const client = new Discord.Client();
-const GCM = new Discord.GuildChannelManager();
 
 client.on("ready", () => {
     console.log(`Bot iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`);
     client.user.setActivity(`Catching a Vibe 🎵`);
 
+    //client.guild.channel.create("Lofi-Chill-Bot",{type: "text", topic: "Chat dedicado ao bot."})
+    //.then(console.log)
+    //.catch(console.error);
+
+    comando(client, 'ping', async message => {
+        if (message.channel.name === 'lofi-chill-bot'){
+            const m = await message.channel.send("Ping?");
+            m.edit(`Pong! A Latência é de: ${m.createdTimestamp - message.createdTimestamp}ms.`);
+        }
+    })
     
 });
 client.on("guildCreate", guild => {
     console.log(`O Bot entrou no servidor: ${guild.name} (id: ${guild.id}), com ${guild.memberCount} pessoas!`);
-    client.on("message", async message => {
-        message.channel.send("Para saber meus comandos Digite /comandos!");
-    });
-    GCM.create("Lofi-Chill-Bot",{topic: "Chat dedicado ao bot."})
-    .then(console.log)
-    .catch(console.error);
+    
+
 
 });
 
 client.on("guildDelete", guild => {
     console.log(`O Bot foi removido do servidor: ${guild.name} (id: ${guild.id})`)
-}); 
-
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-    
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-    const comando = args.shift().toLowerCase();
-
-    if (comando === "comandos"){
-        message.channel.send
-    (`
-    Meus Comandos são:
-        /ping
-        /comandos
-
-    `);
-    }
-
-    if (comando === "ping") {
-        const m = await message.channel.send("Ping?");
-        m.edit(`Pong! A Latência é de: ${m.createdTimestamp - message.createdTimestamp}ms.`);
-    }
-    
 }); 
 
 client.login(config.token);
