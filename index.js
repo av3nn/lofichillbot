f7cb63170cc33b5b6f2f87ec4817123993c0b924
BM <bruno.mafp@gmail.com>
2021-07-14 16:32:47 -0300

    inserção do ytdl

--- index.js
+++ index.js
@@ -10,31 +10,40 @@
     */
 
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require('./config.json');
+const { chatname } = require('./config.json');
 const comando = require("./comandos.js");
-
 const client = new Discord.Client();
+
 
 client.on("ready", () => {
     console.log(`Bot iniciado, com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores.`);
-    client.user.setActivity(`Catching a Vibe 🎵`);
+    
+    client.user.setActivity(`Catching a Vibe 🎵`);  
 
-    //client.guild.channel.create("Lofi-Chill-Bot",{type: "text", topic: "Chat dedicado ao bot."})
-    //.then(console.log)
-    //.catch(console.error);
-
-    comando(client, 'ping', async message => {
-        if (message.channel.name === 'lofi-chill-bot'){
-            const m = await message.channel.send("Ping?");
-            m.edit(`Pong! A Latência é de: ${m.createdTimestamp - message.createdTimestamp}ms.`);
-        }
+    comando(client, 'ping', async message => {   
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! A Latência é de: ${m.createdTimestamp - message.createdTimestamp}ms.`);
     })
     
+    comando(client, 'comandos',  message => {   
+        message.channel.send
+        ("Comandos disponíveis: \n" 
+        + "!comandos (Mostra todos os comandos disponíveis)\n"
+        + "!ping (Mostra o ping entre você e o bot)\n"
+        + "!tocar <> \n"
+        + "!ping \n"
+
+        );
+    })
+
 });
 client.on("guildCreate", guild => {
     console.log(`O Bot entrou no servidor: ${guild.name} (id: ${guild.id}), com ${guild.memberCount} pessoas!`);
     
-
+    const textchat = guild.channels.create(chatname, {type: "text", topic: "Chat Dedicado ao Lofi Chill Bot! Utilize !comandos para obter ajuda."})
+    .then(console.log)
+    .catch(console.error);   
 
 });
 
